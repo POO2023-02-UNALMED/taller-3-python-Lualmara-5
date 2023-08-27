b571762b15bc17944cc73481a87ed597c020cf95
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-27 12:11:10 -0500

    Class TV Part 2

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,1 +1,12 @@
 class TV:
+
+    _numTV = 0
+    def __init__(self, marca, estado):
+
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._volumen = 1
+        self._precio = 500
+        self._control = None
+        TV._numTV += 1
