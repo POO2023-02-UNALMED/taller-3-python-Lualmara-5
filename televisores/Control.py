c175316869768a07d442c126a8effc803e632b98
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-27 15:10:45 -0500

    Posible Solucion

--- televisores/Control.py
+++ televisores/Control.py
@@ -1,45 +1,70 @@
-class Control:
-    def __init__(self):
-        self._tv = None
+class TV:
+    _numTV = 0
+    def __init__(self, marca, estado):
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._volumen = 1
+        self._precio = 500
+        self._control = None
+        TV._numTV += 1
 
-    def enlazar(self, tv):
-        tv.setControl(self)
-        self.setTv(tv)
-
-    def getTv(self):
-        return self._tv
+    def setMarca(self, marca):
+        self._marca = marca
+    def getMarca(self):
+        return self._marca
     
-    def setTv(self, tv):
-        self._tv = tv
+    def setCanal(self, canal):
+        if self._estado:
+            if 1 <= canal and canal <= 120:
+                self._canal = canal
+    def getCanal(self):
+        return self._canal
 
+    def setPrecio(self, precio):
+        self._precio = precio
+    def getPrecio(self):
+        return self._precio
+
+    def setVolumen(self, volumen):
+        if self._estado:
+            if 0 <= volumen and volumen <= 7:
+                self._volumen = volumen
+    def getVolumen(self):
+        return self._volumen
+    
+    def setControl(self, control):
+        self._control = control
+    def getControl(self):
+        return self._control
+    
+    def setNumTV(numTV):
+        TV._numTV = numTV
+    def getNumTV():
+        return TV._numTV
+    
     def turnOn(self):
-        if self._tv != None:
-            self._tv.turnOn()
-
+        self._estado = True
     def turnOff(self):
-        if self._tv != None:
-            self._tv.turnOff() 
+        self._estado = False
 
+    def getEstado(self):
+        return self._estado
+    
     def canalUp(self):
-        if self._tv != None:
-            self._tv.canalUp()
-
+        if self._estado:
+            if self._canal < 120:
+                self._canal += 1
     def canalDown(self):
-        if self._tv != None:
-            self._tv.canalDown()
+        if self._estado:
+            if self._canal > 1:
+                self._canal -= 1 
 
     def volumenUp(self):
-        if self._tv != None:
-            self._tv.volumenUp()
-
+        if self._estado:
+            if self._volumen < 7:
+                self._volumen += 1
     def volumenDown(self):
-        if self._tv != None:
-            self._tv.volumenDown()
-
-    def setCanal(self, canal):
-        if self._tv != None:
-            self._tv.setCanal(canal)
-        
-    def setVolumen(self, volumen):
-        if self._tv != None:
-            self._tv.setVolumen(volumen)
+        if self._estado:
+            if self._volumen > 0:
+                self._volumen -= 1
